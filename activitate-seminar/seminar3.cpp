c6aeb857d061be95f5417525cad428644b786f87
nastureliustin23 <nastureliustin23@stud.ase.ro>
2024-10-17 14:47:06 +0300

    Final seminar 3

--- activitate-seminar/seminar3.cpp
+++ activitate-seminar/seminar3.cpp
@@ -22,6 +22,29 @@ public:
 		cout << "TVA: " << this->TVA << endl;
 		cout << "anFabricatie: " << this->anFabricatie << endl;
 	}
+	Masina(int _nrRoti, string marca, float pret, int an):anFabricatie(an) {
+		this->nrRoti = _nrRoti;
+		this->Marca = marca;
+		this->Pret = pret;
+	}
+
+	float calculeazaPretTotal() {
+		return this->Pret + (this->Pret * (this->TVA / 100.0f));
+	}
+
+	static void modificaTVA(int noulTVA) {
+		if (noulTVA >= 0) {
+			Masina::TVA = noulTVA;
+		}
+	}
+
+	static float calculeazaPretTotalVector(int nrMasini, Masina* vector) {
+		float pretTotalVector = 0;
+		for (int i = 0; i < nrMasini; i++) {
+			pretTotalVector += vector[i].calculeazaPretTotal();
+		}
+		return pretTotalVector;
+	}
 
 };
 
@@ -29,7 +52,32 @@ int Masina::TVA = 19;
 
 int main()
 {
-	Masina m;
-	m.afisare();
+	Masina m1;
+	m1.afisare();
+	
+	cout << endl;
+
+	Masina m2(4, "Audi", 7000, 2020);
+	m2.afisare();
+
+	cout << endl;
+
+	Masina::modificaTVA(21);
+
+	Masina* pointer = new Masina(6, "Hummer", 55000, 1998);
+	pointer->afisare();
+
+	cout << "Pret total:" << pointer->calculeazaPretTotal() << endl<<endl;
+
+	int nrMasini = 3;
+	Masina* vector;
+	vector = new Masina[nrMasini];
+
+	for (int i = 0; i < nrMasini; i++) {
+		vector[i].afisare();
+		cout << endl;
+	}
+	cout << "Pret total flota: " << Masina::calculeazaPretTotalVector(nrMasini, vector) << endl;
+
 	return 1000;
 }
