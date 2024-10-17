cb93e630bfb75b7855e7705f8a9e9973b0160a46
nastureliustin23 <nastureliustin23@stud.ase.ro>
2024-10-17 14:00:34 +0300

    Seminar 3

--- activitate-seminar/seminar3.cpp
+++ activitate-seminar/seminar3.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
 using namespace std;
 
+class Masina {
+public:
+	int nrRoti;
+	string Marca;
+	float Pret;
+	static int TVA;
+	const int anFabricatie;
+
+	Masina():anFabricatie(2024) {
+		this->nrRoti = 4;
+		this->Marca = "Dacia";
+		this->Pret = 5000;
+	}
+
+	void afisare() {
+		cout << "Marca: " << this->Marca << endl;
+		cout << "Numar roti: " << this->nrRoti << endl;
+		cout << "Pret: " << this->Pret << endl;
+		cout << "TVA: " << this->TVA << endl;
+		cout << "anFabricatie: " << this->anFabricatie << endl;
+	}
+
+};
+
+int Masina::TVA = 19;
+
 int main()
 {
-	cout << "Doar de proba!";
-	return 0;
+	Masina m;
+	m.afisare();
+	return 1000;
 }
